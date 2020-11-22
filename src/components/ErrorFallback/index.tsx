34da141779239f0a50b7fc02c47060daa98e131c
Basant Soni <basantech89@gmail.com>
2020-11-22 17:58:21 +0530

    add useAsync hook and ErrorFallback component

--- src/components/ErrorFallback/index.tsx
+++ src/components/ErrorFallback/index.tsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import { FallbackProps } from 'react-error-boundary'
+import { useErrorFallbackStyles } from './style'
 
 const ErrorFallback: React.FC<FallbackProps> = ({ error }) => {
+	const classes = useErrorFallbackStyles()
+
 	if (error) {
 		return (
 			<div>
 				There was an error:{' '}
-				<pre style={{ whiteSpace: 'normal' }}> {error.message}</pre>
+				<pre className={classes.errorMess}> {error.message}</pre>
 			</div>
 		)
 	}
+
 	return null
 }
 
